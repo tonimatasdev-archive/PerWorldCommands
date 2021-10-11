aa9cf83964080e6a42158a813d584ee6ada34423
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2021-10-11 19:18:05 +0200

    Updated: MessageManager.java

--- src/net/tonimatasmc/perworldcommands/manager/MessageManager.java
+++ src/net/tonimatasmc/perworldcommands/manager/MessageManager.java
@@ -7,7 +7,7 @@ import org.bukkit.ChatColor;
 public class MessageManager {
     public static void registerMessage() {
         Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "<---------------------------------------->");
-        Bukkit.getConsoleSender().sendMessage(PerWorldCommands.plugin.prefix + ChatColor.DARK_GREEN + " The plugin was activated (Version: " + PerWorldCommands.plugin.version + ")");
+        Bukkit.getConsoleSender().sendMessage(PerWorldCommands.getPlugin().prefix + ChatColor.DARK_GREEN + " The plugin was activated (Version: " + PerWorldCommands.getPlugin().version + ")");
         Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_AQUA + "Visit: https://tonimatasmc.com");
         Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "<---------------------------------------->");
     }
