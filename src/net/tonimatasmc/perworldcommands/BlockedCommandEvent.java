e03ff1191a30542fc9625ec3bbeef557fd286622
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2021-10-11 18:02:48 +0200

    Updated: BlockedCommandEvent.java

--- src/net/tonimatasmc/perworldcommands/BlockedCommandEvent.java
+++ src/net/tonimatasmc/perworldcommands/BlockedCommandEvent.java
@@ -1,19 +1,15 @@
 package net.tonimatasmc.perworldcommands;
 
+import net.tonimatasmc.perworldcommands.manager.CommandManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class BlockedCommandEvent implements Listener {
-    private final PerWorldCommands plugin;
-
-    public BlockedCommandEvent(PerWorldCommands plugin) {
-        this.plugin = plugin;
-    }
 
     @EventHandler
     public void PerWorldCommand(PlayerCommandPreprocessEvent event) {
-        if (this.plugin.Command(event.getMessage().substring(1), event.getPlayer())) {
+        if (CommandManager.constructor(event.getMessage().substring(1), event.getPlayer())) {
             event.setCancelled(true);
         }
 
