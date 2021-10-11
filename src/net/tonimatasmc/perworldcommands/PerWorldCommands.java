d2953735842bc17eb2a1c714313c08d6e7607717
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2021-10-11 19:18:17 +0200

    Updated: PerWorldCommands.java

--- src/net/tonimatasmc/perworldcommands/PerWorldCommands.java
+++ src/net/tonimatasmc/perworldcommands/PerWorldCommands.java
@@ -5,13 +5,18 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PerWorldCommands extends JavaPlugin implements Listener {
-    public static PerWorldCommands plugin;
+    private static PerWorldCommands plugin;
     public String version;
     public String prefix;
 
     public void onEnable() {
         this.version = this.getDescription().getVersion();
         this.prefix = this.getDescription().getPrefix();
+        plugin = this;
         RegisterManager.register();
+    }
+
+    public static PerWorldCommands getPlugin() {
+        return plugin;
     }
 }
