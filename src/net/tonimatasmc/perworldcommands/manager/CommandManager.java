ec21841484f13002bd94f971f5ff0f84851a74bd
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2021-10-11 19:17:37 +0200

    Updated: CommandManager.java

--- src/net/tonimatasmc/perworldcommands/manager/CommandManager.java
+++ src/net/tonimatasmc/perworldcommands/manager/CommandManager.java
@@ -6,17 +6,17 @@ import org.bukkit.entity.Player;
 
 public class CommandManager {
     public static boolean constructor(String message, Player player) {
-        if (PerWorldCommands.plugin.getConfig().getConfigurationSection("commands." + message.toLowerCase()) != null) {
-            String msg = PerWorldCommands.plugin.getConfig().getString("globalblockmessage");
+        if (PerWorldCommands.getPlugin().getConfig().getConfigurationSection("commands." + message.toLowerCase()) != null) {
+            String msg = PerWorldCommands.getPlugin().getConfig().getString("globalblockmessage");
 
-            if (PerWorldCommands.plugin.getConfig().getString("commands." + message + ".blockmessage") != null) {
-                msg = PerWorldCommands.plugin.getConfig().getString("commands." + message + ".blockmessage");
+            if (PerWorldCommands.getPlugin().getConfig().getString("commands." + message + ".blockmessage") != null) {
+                msg = PerWorldCommands.getPlugin().getConfig().getString("commands." + message + ".blockmessage");
             }
 
-            if (PerWorldCommands.plugin.getConfig().getStringList("commands." + message + ".allowed-worlds").contains(player.getWorld().getName().toLowerCase())) {
+            if (PerWorldCommands.getPlugin().getConfig().getStringList("commands." + message + ".allowed-worlds").contains(player.getWorld().getName().toLowerCase())) {
                 return false;
             } else {
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', PerWorldCommands.plugin.getConfig().getString("Prefix")+ " " + msg));
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', PerWorldCommands.getPlugin().getConfig().getString("Prefix")+ " " + msg));
                 return true;
             }
         } else {
