bf1b2bc30c929ccfa40a687cec1b0b4aea66c216
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2021-10-21 19:57:18 +0200

    Updated: MessageManager.java

--- src/net/tonimatasmc/perworldcommands/manager/MessageManager.java
+++ src/net/tonimatasmc/perworldcommands/manager/MessageManager.java
@@ -7,8 +7,15 @@ import org.bukkit.ChatColor;
 public class MessageManager {
     public static void registerMessage() {
         Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "<---------------------------------------->");
-        Bukkit.getConsoleSender().sendMessage(PerWorldCommands.getPlugin().prefix + ChatColor.DARK_GREEN + " The plugin was activated (Version: " + PerWorldCommands.getPlugin().version + ")");
-        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_AQUA + "Visit: https://tonimatasmc.com");
+        Bukkit.getConsoleSender().sendMessage(PerWorldCommands.getPlugin().prefix + ChatColor.DARK_GREEN + " The plugin has been enabled (Version: " + PerWorldCommands.getPlugin().version + ")");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_AQUA + "Visit: " + ChatColor.DARK_BLUE + "https://tonimatasmc.com");
         Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "<---------------------------------------->");
+    }
+
+    public static void unregisterMessage() {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_RED + "<---------------------------------------->");
+        Bukkit.getConsoleSender().sendMessage(PerWorldCommands.getPlugin().prefix + ChatColor.DARK_RED + " The plugin has been disabled (Version: " + PerWorldCommands.getPlugin().version + ")");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_RED + "Visit: " + ChatColor.DARK_BLUE + "https://tonimatasmc.com");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_RED + "<---------------------------------------->");
     }
 }
