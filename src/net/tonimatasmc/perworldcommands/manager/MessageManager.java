aa6c4cee1740ccaea769cf3f4992ab63e0a82b61
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2021-11-13 11:18:57 +0100

    Updated: MessageManager.java

--- src/net/tonimatasmc/perworldcommands/manager/MessageManager.java
+++ src/net/tonimatasmc/perworldcommands/manager/MessageManager.java
@@ -1,20 +1,20 @@
 package net.tonimatasmc.perworldcommands.manager;
 
-import net.tonimatasmc.perworldcommands.PerWorldCommands;
+import net.tonimatasmc.perworldcommands.utils.PluginDescription;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
 public class MessageManager {
     public static void registerMessage() {
         Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "<---------------------------------------->");
-        Bukkit.getConsoleSender().sendMessage(PerWorldCommands.getPlugin().prefix + ChatColor.DARK_GREEN + " The plugin has been enabled (Version: " + PerWorldCommands.getPlugin().version + ")");
+        Bukkit.getConsoleSender().sendMessage(PluginDescription.prefix + ChatColor.DARK_GREEN + " The plugin has been enabled (Version: " + PluginDescription.prefix + ")");
         Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_AQUA + "Visit: " + ChatColor.DARK_BLUE + "https://tonimatasmc.com");
         Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "<---------------------------------------->");
     }
 
     public static void unregisterMessage() {
         Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_RED + "<---------------------------------------->");
-        Bukkit.getConsoleSender().sendMessage(PerWorldCommands.getPlugin().prefix + ChatColor.DARK_RED + " The plugin has been disabled (Version: " + PerWorldCommands.getPlugin().version + ")");
+        Bukkit.getConsoleSender().sendMessage(PluginDescription.prefix + ChatColor.DARK_RED + " The plugin has been disabled (Version: " + PluginDescription.prefix + ")");
         Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_RED + "Visit: " + ChatColor.DARK_BLUE + "https://tonimatasmc.com");
         Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_RED + "<---------------------------------------->");
     }
