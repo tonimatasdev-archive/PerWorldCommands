93717c560b1a34a908063d3be3b2379806c325be
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2021-11-13 11:20:09 +0100

    Updated: UpdateChecker.java

--- src/net/tonimatasmc/perworldcommands/utils/UpdateChecker.java
+++ src/net/tonimatasmc/perworldcommands/utils/UpdateChecker.java
@@ -14,17 +14,17 @@ public class UpdateChecker {
 
     public static void check() {
         try {
-            HttpURLConnection con = (HttpURLConnection) (new URL("https://api.spigotmc.org/legacy/update.php?resource=96185")).openConnection();
+            HttpURLConnection con = (HttpURLConnection) (new URL("https://api.spigotmc.org/legacy/update.php?resource=97003")).openConnection();
             int timed_out = 1250;
             con.setConnectTimeout(timed_out);
             con.setReadTimeout(timed_out);
             latestversion = (new BufferedReader(new InputStreamReader(con.getInputStream()))).readLine();
-            if (latestversion.length() <= 7 && !PerWorldCommands.getPlugin().version.equals(latestversion)) {
-                Bukkit.getConsoleSender().sendMessage(PerWorldCommands.getPlugin().prefix + ChatColor.RED + " There is a new version available. " + ChatColor.YELLOW + "(" + ChatColor.GRAY + latestversion + ChatColor.YELLOW + ")");
-                Bukkit.getConsoleSender().sendMessage(PerWorldCommands.getPlugin().prefix + ChatColor.RED + " You can download it at: " + ChatColor.WHITE + "https://www.spigotmc.org/resources/instakillmobsgamemode.96185");
+            if (latestversion.length() <= 7 && !PluginDescription.version.equals(latestversion)) {
+                Bukkit.getConsoleSender().sendMessage(PluginDescription.prefix + ChatColor.RED + " There is a new version available. " + ChatColor.YELLOW + "(" + ChatColor.GRAY + latestversion + ChatColor.YELLOW + ")");
+                Bukkit.getConsoleSender().sendMessage(PluginDescription.prefix + ChatColor.RED + " You can download it at: " + ChatColor.WHITE + "https://www.spigotmc.org/resources/perworldcommands.97003/");
             }
         } catch (Exception var3) {
-            Bukkit.getConsoleSender().sendMessage(PerWorldCommands.getPlugin().prefix + ChatColor.RED + " Error while checking update.");
+            Bukkit.getConsoleSender().sendMessage(PluginDescription.prefix + ChatColor.RED + " Error while checking update.");
         }
     }
 }
