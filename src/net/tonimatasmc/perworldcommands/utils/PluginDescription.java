fbbf0f624429f9370ca2a58ef645570be9b34f34
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2021-12-04 19:54:04 +0100

    Updated: PluginDescription.java

--- src/net/tonimatasmc/perworldcommands/utils/PluginDescription.java
+++ src/net/tonimatasmc/perworldcommands/utils/PluginDescription.java
@@ -12,7 +12,6 @@ public class PluginDescription {
     public static String description;
     public static String softDepend;
     public static String loadBefore;
-    public static String awareness;
     public static String commands;
     public static String fullname;
     public static String authors;
@@ -31,7 +30,6 @@ public class PluginDescription {
         main = pdffile.getMain();
         commands = String.valueOf(pdffile.getCommands());
         permissions = String.valueOf(pdffile.getPermissions());
-        awareness = String.valueOf(pdffile.getAwareness());
         description = pdffile.getDescription();
         authors = String.valueOf(pdffile.getAuthors());
         depend = String.valueOf(pdffile.getDepend());
