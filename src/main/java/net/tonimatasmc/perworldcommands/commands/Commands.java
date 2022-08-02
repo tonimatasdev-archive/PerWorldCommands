64cc9088b28cc87982c5f59ff05da0ac50d46b48
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2022-08-02 12:33:30 +0200

    Command fixes

--- src/main/java/net/tonimatasmc/perworldcommands/commands/Commands.java
+++ src/main/java/net/tonimatasmc/perworldcommands/commands/Commands.java
@@ -9,68 +9,73 @@ import org.bukkit.command.CommandSender;
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings({"ConstantConditions"})
 public class Commands implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("pwc") || cmd.getName().equalsIgnoreCase("perworldcommands")) {
             if (sender.hasPermission("perworldcommands.cmd") || sender.hasPermission("pwc.cmd")) {
-                if (args[0].equalsIgnoreCase("set")) {
-                    if (args[1].equalsIgnoreCase("cmd")) {
-                        List<String> worldList = new ArrayList<>();
-                        worldList.add(args[3]);
+                if (args.length >= 1) {
+                    if (args[0].equalsIgnoreCase("set")) {
+                        if (args.length >= 2) {
+                            if (args[1].equalsIgnoreCase("cmd")) {
+                                List<String> worldList = new ArrayList<>();
+                                if (args.length >= 4) {
+                                    worldList.add(args[3]);
 
-                        if (args.length <= 3) {
-                            worldList.add(args[4]);
+                                    if (args.length >= 5) {
+                                        worldList.add(args[4]);
 
-                            if (args.length <= 4) {
-                                worldList.add(args[5]);
+                                        if (args.length >= 6) {
+                                            worldList.add(args[5]);
 
-                                if (args.length <= 5) {
-                                    worldList.add(args[6]);
+                                            if (args.length >= 7) {
+                                                worldList.add(args[6]);
 
-                                    if (args.length <= 6) {
-                                        worldList.add(args[7]);
+                                                if (args.length >= 8) {
+                                                    worldList.add(args[7]);
 
-                                        if (args.length <= 7) {
-                                            worldList.add(args[8]);
+                                                    if (args.length >= 9) {
+                                                        worldList.add(args[8]);
 
-                                            if (args.length <= 8) {
-                                                worldList.add(args[9]);
+                                                        if (args.length >= 10) {
+                                                            worldList.add(args[9]);
 
-                                                if (args.length <= 9) {
-                                                    worldList.add(args[10]);
+                                                            if (args.length >= 11) {
+                                                                worldList.add(args[10]);
 
-                                                    if (args.length <= 10) {
-                                                        worldList.add(args[11]);
+                                                                if (args.length >= 12) {
+                                                                    worldList.add(args[11]);
 
-                                                        if (args.length <= 11) {
-                                                            worldList.add(args[12]);
+                                                                    if (args.length >= 13) {
+                                                                        worldList.add(args[12]);
 
-                                                            if (args.length <= 12) {
-                                                                worldList.add(args[13]);
+                                                                        if (args.length >= 14) {
+                                                                            worldList.add(args[13]);
 
-                                                                if (args.length <= 13) {
-                                                                    worldList.add(args[14]);
+                                                                            if (args.length >= 15) {
+                                                                                worldList.add(args[14]);
 
-                                                                    if (args.length <= 14) {
-                                                                        worldList.add(args[15]);
+                                                                                if (args.length >= 16) {
+                                                                                    worldList.add(args[15]);
 
-                                                                        if (args.length <= 15) {
-                                                                            worldList.add(args[16]);
+                                                                                    if (args.length >= 17) {
+                                                                                        worldList.add(args[16]);
 
-                                                                            if (args.length <= 16) {
-                                                                                worldList.add(args[17]);
+                                                                                        if (args.length >= 18) {
+                                                                                            worldList.add(args[17]);
 
-                                                                                if (args.length <= 17) {
-                                                                                    worldList.add(args[18]);
+                                                                                            if (args.length >= 19) {
+                                                                                                worldList.add(args[18]);
 
-                                                                                    if (args.length <= 18) {
-                                                                                        worldList.add(args[19]);
+                                                                                                if (args.length >= 20) {
+                                                                                                    worldList.add(args[19]);
 
-                                                                                        if (args.length <= 19) {
-                                                                                            worldList.add(args[20]);
+                                                                                                    if (args.length >= 21) {
+                                                                                                        worldList.add(args[20]);
+                                                                                                    }
+                                                                                                }
+                                                                                            }
                                                                                         }
                                                                                     }
                                                                                 }
@@ -85,39 +90,63 @@ public class Commands implements CommandExecutor {
                                             }
                                         }
                                     }
+                                } else {
+                                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigManager.getMessages().getString("SyntaxError")));
+                                }
+
+                                if (args.length >= 3) {
+                                    PerWorldCommands.getPlugin().getConfig().set("commands." + args[2] + ".allowed-worlds", worldList);
+                                    PerWorldCommands.getPlugin().saveConfig();
+                                    PerWorldCommands.getPlugin().reloadConfig();
+                                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigManager.getMessages().getString("AddCommand").replace("%command%", args[2])));
+                                } else {
+                                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigManager.getMessages().getString("SyntaxError")));
                                 }
                             }
+
+                            if (args[1].equalsIgnoreCase("msg")) {
+                                if (args.length == 3) {
+                                    PerWorldCommands.getPlugin().getConfig().set("globalblockmessage", args[2]);
+                                    PerWorldCommands.getPlugin().saveConfig();
+                                    PerWorldCommands.getPlugin().reloadConfig();
+                                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigManager.getMessages().getString("MessageApplicated").replace("%message%", args[2])));
+                                } else {
+                                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigManager.getMessages().getString("SyntaxError")));
+                                }
+                            }
+                        } else {
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigManager.getMessages().getString("SyntaxError")));
                         }
-
-                        PerWorldCommands.getPlugin().getConfig().set("commands." + args[2] + ".allowed-worlds", worldList);
-                        PerWorldCommands.getPlugin().saveConfig();
-                        PerWorldCommands.getPlugin().reloadConfig();
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigManager.getMessages().getString("AddCommand").replace("%command%", args[2])));
                     }
 
-                    if (args[1].equalsIgnoreCase("msg")) {
-                        PerWorldCommands.getPlugin().getConfig().set("globalblockmessage", "\"" + args[2] + "\"");
-                        PerWorldCommands.getPlugin().saveConfig();
-                        PerWorldCommands.getPlugin().reloadConfig();
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigManager.getMessages().getString("MessageApplicated").replace("%message%", "\"" + (args.length <= 2) + "\"")));
+                    if (args[0].equalsIgnoreCase("remove")) {
+                        if (args.length >= 2) {
+                            if (args[1].equalsIgnoreCase("cmd")) {
+                                if (args.length == 3) {
+                                    PerWorldCommands.getPlugin().getConfig().set("commands." + args[2], null);
+                                    PerWorldCommands.getPlugin().saveConfig();
+                                    PerWorldCommands.getPlugin().reloadConfig();
+                                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigManager.getMessages().getString("RemoveCommand").replace("%command%", args[2])));
+                                } else {
+                                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigManager.getMessages().getString("SyntaxError")));
+                                }
+                            }
+                        } else {
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigManager.getMessages().getString("SyntaxError")));
+                        }
                     }
-                }
 
-                if (args[0].equalsIgnoreCase("remove")) {
-                    if (args[1].equalsIgnoreCase("cmd")) {
-                        PerWorldCommands.getPlugin().getConfig().set("commands." + args[2], null);
-                        PerWorldCommands.getPlugin().saveConfig();
+                    if (args[0].equalsIgnoreCase("reload")) {
                         PerWorldCommands.getPlugin().reloadConfig();
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigManager.getMessages().getString("RemoveCommand").replace("%command%", args[2])));
+                        ConfigManager.reloadMessages();
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigManager.getMessages().getString("ReloadPlugin")));
                     }
-                }
-
-                if (args[0].equalsIgnoreCase("reload")) {
-                    PerWorldCommands.getPlugin().reloadConfig();
-                    ConfigManager.reloadMessages();
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigManager.getMessages().getString("ReloadPlugin")));
+                } else {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigManager.getMessages().getString("SyntaxError")));
                 }
             }
-        }return true;
+        }
+
+        return true;
     }
 }
